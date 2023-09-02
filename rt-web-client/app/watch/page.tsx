cb0cee534e8fafd597a852bbe00ee4560f04cd1f
Ian Liu <ianliu@livemail.tw>
2023-09-02 09:26:53 +0800

    add loading screen and add use useEffect

--- rt-web-client/app/watch/page.tsx
+++ rt-web-client/app/watch/page.tsx
@@ -3,27 +3,50 @@
 import Video from '@/app/interfaces/Video';
 import { redirect, useSearchParams } from 'next/navigation'
 import { getVideo } from '../utilities/firebase/functions';
-import { use } from "react";
+import { useEffect, useState } from 'react';
 
 export default function Watch() {
   const videoPrefix = 'https://storage.googleapis.com/retro-tube-processed-videos/';
   const videoId = useSearchParams().get('v');
-  if (!videoId) {
-    return redirect('/');
-  }
-  const video = use(getVideo(videoId)) as Video;
+  const [video, setVideo] = useState<Video | null>(null);
+  const [loadingVideo, setLoadingVideo] = useState(true)
+
+  
+  useEffect(() => {
+    async function fetchVideo() {
+      if (!videoId) {
+        return;
+      }
+      try {
+        const videoData = await getVideo(videoId);
+        setVideo(videoData as Video);
+      } catch (error) {
+        console.error('Error fetching video:', error);
+        setVideo(null)
+        // Handle error as needed, e.g., set an error state
+      } finally {
+        setLoadingVideo(false);
+      }
+    }
+    fetchVideo();
+  }, [videoId]);
   const videoSrc = video?.filename ?? "";
   
+  if (!videoId) {
+    redirect('/');
+  }
+  if (loadingVideo) {
+    return <h1> Loading... </h1>
+  }
+  if (!video) {
+    return <h1>404 Not Found</h1>;
+  }
   return (
-    <div>
-      <h1>Watch Page</h1>
-      {
-        video ? (
-          <video controls src={videoPrefix + videoSrc} />
-        )
-        : 
-        ( <h3>404 Not Found</h3> )
-      }
-    </div>
+    <main>
+      <h1>{video.title}</h1>
+      <video controls src={videoPrefix + videoSrc} />
+      <h3> Description: </h3>
+      <div> {video.description} </div>
+    </main>
   );
 }
