77976a9911691551ea03d3aafb0a108bbbda8839
Ian Liu <ianliu@livemail.tw>
2023-09-03 10:48:53 +0800

    make upload button render in Safari

--- rt-web-client/app/navbar/upload.tsx
+++ rt-web-client/app/navbar/upload.tsx
@@ -5,7 +5,8 @@ export default function Upload() {
     return (
         <Fragment>
             <a className={styles.uploadButton} href="/upload">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
+            {/* need to specify height or width to make the svg render in Safari (current version 16.6)*/}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6" width="551">
                     <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
                 </svg>
             </a>
